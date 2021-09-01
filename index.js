5cabf15b7f530a6b4e81c644c6f5265b79e9d775
JoshMTM <joshmitima08@yahoo.com>
2021-09-01 13:49:43 +0200

    changes again

---
+++ index.js
@@ -0,0 +1,3 @@
+const encourage =
+  "Welcome to Codaisseur, this is probably one of the greatest decision you've made in your life. Enjoy your experience coding, it's gonna be an amazing journey!";
+console.log(encourage);
