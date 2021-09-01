641c8dc3f125711e962736e4a603c4d2ea0eafe7
JoshMTM <joshmitima08@yahoo.com>
2021-09-01 13:52:32 +0200

    add a consoleLog

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const encourage =
   "Welcome to Codaisseur, this is probably one of the greatest decision you've made in your life. Enjoy your experience coding, it's gonna be an amazing journey!";
 console.log(encourage);
+
+console.log("good Job");
